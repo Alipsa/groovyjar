e84e12bde527393108dfaa4d1d3684ffbeb6ea55
per <per@alipsa.se>
2025-02-22 23:31:17 +0100

    improve support for class arguments

--- src/main/groovy/se/alipsa/groovy/Greeting.groovy
+++ src/main/groovy/se/alipsa/groovy/Greeting.groovy
@@ -4,7 +4,7 @@ class Greeting {
 
   static void main(String[] args) {
     if (args.length > 0) {
-      println "Hello, args are ${String.join(',', args)}"
+      println "Hello, the $args.length args are ${String.join(',', args)}"
     } else {
       println "Hello"
     }
